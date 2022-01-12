16a334609572444c8a4b74c63f11e9e66ee3baaf
John Blåber Kristoffersson <John.kristoffersson@gmail.com>
2022-01-12 21:56:57 +0100

    style: add einride eslint

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,13 @@
-import * as React from "react"
+import * as React from "react";
 
-const About = () =>{
-    return(
-        <main>
-            <title>About Me</title>
-            <h1>About Me</h1>
-            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-        </main>
-    )
-}
+const About = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  );
+};
 
-export default About
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import * as React from "react"
-import {Link} from "gatsby";
+import * as React from "react";
+import { Link } from "gatsby";
 
 // markup
 const IndexPage = () => {
-    return (
-        <main>
-            <title>Home Page</title>
-            <div className={"bg-blue-400"}>
-                <h1>Welcome to my Gatsby site!</h1>
-                <Link to="/about">About</Link>
-                <p>I'm making this by following the Gatsby Tutorial.</p>
-            </div>
-        </main>
-    )
-}
+  return (
+    <main>
+      <title>Home Page</title>
+      <div className="bg-blue-400">
+        <h1>Welcome to my Gatsby site!</h1>
+        <Link to="/about">About</Link>
+        <p>I&apos;m making this by following the Gatsby Tutorial.</p>
+      </div>
+    </main>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
