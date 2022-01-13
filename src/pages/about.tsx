eaf539efc575502271c281a9a57406bc54c758f4
John Blåber Kristoffersson <John.kristoffersson@gmail.com>
2022-01-13 20:29:06 +0100

    feay: add Layout to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,15 @@
 import * as React from "react"
+import Layout from "../components/Layout"
 
 const About = () => {
   return (
-    <main>
-      <title>About Me</title>
+    <Layout pageTitle="About">
       <h1>About Me</h1>
       <p>
         Hi there! I&apos;m the proud creator of this site, which I built with
         Gatsby.
       </p>
-    </main>
+    </Layout>
   )
 }
 
