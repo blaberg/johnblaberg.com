b44fa5396d70366371da22335cb5130c843800b9
John Blåber Kristoffersson <John.kristoffersson@gmail.com>
2022-01-13 20:24:45 +0100

    feat: add resume and contact pages

---
+++ src/pages/contact.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+
+const Contact = () => {
+  return (
+    <Layout pageTitle="Contact">
+      <p>Contact</p>
+    </Layout>
+  )
+}
+
+export default Contact
---
+++ src/pages/resume.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+
+const Resume = () => {
+  return (
+    <Layout pageTitle="Resume">
+      <p>Resume</p>
+    </Layout>
+  )
+}
+
+export default Resume
