4b7349448bde54bc6a2e61156c4e56d4ce151d95
John Blåber Kristoffersson <John.kristoffersson@gmail.com>
2022-01-13 18:18:18 +0100

    feat: add hero image to blog posts

--- src/pages/{mdx.slug}.tsx
+++ src/pages/{mdx.slug}.tsx
@@ -1,11 +1,15 @@
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import * as React from "react"
 
 const PostTemplate = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+
   return (
     <main>
       <title>{data.mdx.frontmatter.title}</title>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
       <h1>{data.mdx.frontmatter.title}</h1>
       <p>{data.mdx.frontmatter.date}</p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
@@ -16,11 +20,19 @@ const PostTemplate = ({ data }) => {
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `
