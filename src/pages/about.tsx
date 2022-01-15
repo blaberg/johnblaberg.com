cbe41ffabd31a6e8fcfe85305a181d2a1231be28
John Blåber Kristoffersson <John.kristoffersson@gmail.com>
2022-01-15 20:46:47 +0100

    feat: add basic about description

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,10 +4,13 @@ import Layout from "../components/Layout"
 const About = () => {
   return (
     <Layout pageTitle="About">
-      <h1>About Me</h1>
-      <p>
-        Hi there! I&apos;m the proud creator of this site, which I built with
-        Gatsby.
+      <h2 className="font-bold text-3xl mb-6">About</h2>
+      <p className="font-serif text-xl">
+        I&apos;m a student at Chalmers University of technology, studying for a
+        M.Sc in Computer Science and Engineering with a focus on distributed
+        systems and information security. This spring I will be writing my
+        thesis at Einride, which will be focused on the internal network
+        security of autonomous vehicles.
       </p>
     </Layout>
   )
