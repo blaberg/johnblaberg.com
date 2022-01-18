c5c634457ac3a9accf6c3b216822d09e66674726
John Blåber Kristoffersson <John.kristoffersson@gmail.com>
2022-01-18 21:22:45 +0100

    feat: add basic contact info

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,7 +4,32 @@ import Layout from "../components/Layout"
 const Contact = () => {
   return (
     <Layout pageTitle="Contact">
-      <p>Contact</p>
+      <h2 className="font-bold text-3xl mb-6">Contact</h2>
+      <ul>
+        <li>
+          <a href="mailto:john.kristoffersson@gmail.com" className="text-black">
+            john.kirtosffersson@Gmail.com
+          </a>
+        </li>
+        <li>
+          <a href="tel:+46709308018" className="text-black">
+            +46 (0)70 930 80 18
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/blaberg" className="text-black">
+            Github
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/john-kristoffersson/"
+            className="text-black"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </ul>
     </Layout>
   )
 }
