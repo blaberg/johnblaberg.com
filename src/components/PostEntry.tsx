e8ae1469a2c338cf476e3a8241d9c5ea0c800f75
John Blåberg Kristoffersson <John.kristoffersson@gmail.com>
2022-01-30 22:43:52 +0100

    fix: add link to posts

--- src/components/PostEntry.tsx
+++ src/components/PostEntry.tsx
@@ -7,12 +7,12 @@ const PostEntry = ({ node }) => {
 
   return (
     <article className="w-72">
-      <GatsbyImage
-        image={image}
-        alt={node.frontmatter.hero_image_alt}
-        className="w-full h-40 rounded"
-      />
       <Link className="text-black hover:no-underline" to={node.slug}>
+        <GatsbyImage
+          image={image}
+          alt={node.frontmatter.hero_image_alt}
+          className="w-full h-40 rounded"
+        />
         <p className="text-gray-700 font-thin text-sm mt-0.5">
           {node.frontmatter.date}
         </p>
